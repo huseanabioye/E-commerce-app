dc1ba9104c13e62609ea9130356b6e50fb7eae5e
Abioye Husean <abioyehusean2019@gnail.com>
2025-04-28 09:17:55 +0100

    adding edit and delete to app

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\category;
+
+class AdminController extends Controller
+{
+    public function  view_category()
+    {
+        $data = category::all();
+        return view('admin.category', compact('data'));
+    }
+
+    public function add_category(Request $request)
+    {
+        $category = new category;
+
+        $category->category_name = $request->category;
+        $category->save();
+
+        // toastr()->timeOut(10000)->closeButton()->addSuccess('Category
+        // Added Successfully');
+
+        return redirect()->back();
+    }
+
+    public function edit_category($id)
+    {
+        $data = category::find($id);
+        return view('admin.edit_category',compact('data'));
+    }
+
+    public function update_category(Request $request, $id)
+    {
+         $data = category::find($id);
+
+         $data->category_name= $request->category;
+
+         $data->save();
+
+         return redirect('/view_category');
+    }
+
+    public function delete_category($id)
+    {
+        $data = category::find($id);
+        $data->delete();
+
+        // toastr()->timeOut(10000)->closeButton()->addSuccess('Category
+        // Deleted Successfully');
+
+
+        return redirect()->back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'home']);
@@ -19,4 +21,19 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::get('admin/dashboard',[HomeController::class,'index'])->
+middleware(['auth','admin']);
+
+
+Route::get('view_category',[AdminController::class,'view_category'])->
+middleware(['auth','admin']);
+
+Route::post('add_category',[AdminController::class,'add_category'])->
+middleware(['auth','admin']);
+
+Route::get('delete_category/{id}',[AdminController::class,'delete_category'])->
+middleware(['auth','admin']);
+
+Route::get('edit_category/{id}',[AdminController::class,'edit_category'])->
+middleware(['auth','admin']);
+Route::post('update_category/{id}',[AdminController::class,'update_category'])->
 middleware(['auth','admin']);
